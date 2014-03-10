de89953787880455fca886654f80e82c80f60e08
aronnair <arunmakesarun@gmail.com>
2014-03-10 12:19:30 +0530

    Added static graphs for start and end %

--- src/com/iitb/aakash/batterycheck/Graph.java
+++ src/com/iitb/aakash/batterycheck/Graph.java
@@ -3,6 +3,8 @@ package com.iitb.aakash.batterycheck;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint.Align;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -12,9 +14,11 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.iitb.aakash.batterycheck.SimpleGestureFilter.SimpleGestureListener;
+import com.jjoe64.graphview.BarGraphView;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.GraphViewSeries.GraphViewSeriesStyle;
 import com.jjoe64.graphview.LineGraphView;
 
 public class Graph extends Activity implements SimpleGestureListener {
@@ -22,29 +26,31 @@ public class Graph extends Activity implements SimpleGestureListener {
 	TextView txt_info, txt_logs, txt_graph, txtTitle;
 	private SimpleGestureFilter detector;
 	LinearLayout layout_logs, layout_info;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.graph); 
+		setContentView(R.layout.graph);
 		detector = new SimpleGestureFilter(this, this);
 		txt_info = (TextView) findViewById(R.id.txtInfo_inactive);
 		txt_logs = (TextView) findViewById(R.id.txtLogs_inactive);
 		txt_graph = (TextView) findViewById(R.id.txtGraph_active);
 		txtTitle = (TextView) findViewById(R.id.txtTitle);
-		
-		
-		layout_info=(LinearLayout)findViewById(R.id.layoutInfo_inactive);
-		layout_logs=(LinearLayout)findViewById(R.id.layoutLogs_inactive);
-		
-		Typeface font = Typeface.createFromAsset(getAssets(), "JosefinSlab-Light.ttf");
-		Typeface font_bold = Typeface.createFromAsset(getAssets(), "JosefinSlab-SemiBold.ttf");
-		Typeface font_normal = Typeface.createFromAsset(getAssets(), "JosefinSlab-Regular.ttf");
+
+		layout_info = (LinearLayout) findViewById(R.id.layoutInfo_inactive);
+		layout_logs = (LinearLayout) findViewById(R.id.layoutLogs_inactive);
+
+		Typeface font = Typeface.createFromAsset(getAssets(),
+				"JosefinSlab-Light.ttf");
+		Typeface font_bold = Typeface.createFromAsset(getAssets(),
+				"JosefinSlab-SemiBold.ttf");
+		Typeface font_normal = Typeface.createFromAsset(getAssets(),
+				"JosefinSlab-Regular.ttf");
 		txt_info.setTypeface(font);
 		txt_graph.setTypeface(font_normal);
 		txt_logs.setTypeface(font);
 		txtTitle.setTypeface(font_bold);
-		
+
 		layout_info.setOnClickListener(new OnClickListener() {
 
 			@SuppressLint("NewApi")
@@ -73,22 +79,67 @@ public class Graph extends Activity implements SimpleGestureListener {
 			}
 		});
 
-		GraphViewSeries exampleSeries = new GraphViewSeries(
-				new GraphViewData[] { new GraphViewData(1, 2.0d),
-						new GraphViewData(2, 1.5d), new GraphViewData(3, 2.5d),
-						new GraphViewData(4, 1.0d) });
+		/*
+		 * // GRAPH DATA int num = 150; GraphViewData[] data = new
+		 * GraphViewData[num]; double v = 0; for (int i = 0; i < num; i++) { v
+		 * += 0.2; data[i] = new GraphViewData(i, Math.sin(v)); }
+		 * GraphViewSeries seriesSin = new GraphViewSeries("Sine", new
+		 * GraphViewSeriesStyle(Color.BLUE, 3), data);
+		 * 
+		 * // cos curve data = new GraphViewData[num]; v = 0; for (int i = 0; i
+		 * < num; i++) { v += 0.2; data[i] = new GraphViewData(i, Math.cos(v));
+		 * } GraphViewSeries seriesCos = new GraphViewSeries("Cosine", new
+		 * GraphViewSeriesStyle(Color.RED, 3), data);
+		 * 
+		 * GraphView graphView = new LineGraphView(this // context , "" //
+		 * heading ); // add data graphView.addSeries(seriesCos);
+		 * graphView.addSeries(seriesSin);
+		 */
+
+		GraphViewSeries start_percent_series = new GraphViewSeries("Start %",
+				new GraphViewSeriesStyle(Color.RED, 0), new GraphViewData[] {
+						new GraphViewData(1, 10), new GraphViewData(2, 20),
+						new GraphViewData(3, 60), new GraphViewData(4, 30) });
 
 		GraphView graphView = new LineGraphView(this // context
-				, "" // heading
+				, "GraphViewDemo" // heading
 		);
-		graphView.addSeries(exampleSeries); // data
+		graphView.addSeries(start_percent_series); // data
+		
+		GraphViewSeries end_percent_series = new GraphViewSeries("End %",
+				new GraphViewSeriesStyle(Color.GREEN, 0), new GraphViewData[] {
+						new GraphViewData(1, 40), new GraphViewData(2, 70),
+						new GraphViewData(3, 100), new GraphViewData(4, 60) });
+
+	
+		graphView.addSeries(end_percent_series); // data
+
+		// optional - set view port, start=2, size=10
 		graphView.setViewPort(1, 5);
-		graphView.setScrollable(false);
+		graphView.setScalable(false);
+		// optional - legend
+		graphView.setShowLegend(true);
+		graphView.setScrollable(true);
 		// optional - activate scaling / zooming
 		graphView.setScalable(false);
+		graphView.getGraphViewStyle().setLegendBorder(20);
+		graphView.getGraphViewStyle().setLegendSpacing(30);
+		graphView.getGraphViewStyle().setLegendWidth(200);
+		
+		((LineGraphView) graphView).setDrawDataPoints(true);
+		((LineGraphView) graphView).setDataPointsRadius(15f);
+		
+		
+		graphView.getGraphViewStyle().setGridColor(Color.GRAY);
+
+		graphView.getGraphViewStyle().setNumHorizontalLabels(6);
+		graphView.getGraphViewStyle().setNumVerticalLabels(5);
+
+		graphView.getGraphViewStyle().setVerticalLabelsAlign(Align.CENTER);
 
 		LinearLayout layout = (LinearLayout) findViewById(R.id.graph1);
 		layout.addView(graphView);
+
 	}
 
 	@Override
@@ -100,27 +151,21 @@ public class Graph extends Activity implements SimpleGestureListener {
 
 	@Override
 	public void onSwipe(int direction) {
-		
+
 		switch (direction) {
 
 		case SimpleGestureFilter.SWIPE_RIGHT:
-			System.out.println("RIGHT");
-			Intent logsactivity = new Intent(Graph.this, Logs.class);
-			startActivity(logsactivity);
-			overridePendingTransition(R.anim.anim_left_to_right,
-					R.anim.anim_right_to_left);
-			finish();
+
 			break;
 		case SimpleGestureFilter.SWIPE_LEFT:
-			System.out.println("LEFT");
-			
+
 			break;
-		
+
 		}
 	}
 
 	@Override
 	public void onDoubleTap() {
-		//Toast.makeText(this, "Double Tap", Toast.LENGTH_SHORT).show();
+		// Toast.makeText(this, "Double Tap", Toast.LENGTH_SHORT).show();
 	}
 }
