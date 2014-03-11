3a9dc1f9a154a81832ea4392e47231651173fc8a
ninad chilap <ninadchilap@gmail.com>
2014-03-11 17:41:37 +0530

    on boot start service

---
+++ src/com/iitb/aakash/batterycheck/StartMyServiceAtBootReceiver.java
@@ -0,0 +1,17 @@
+package com.iitb.aakash.batterycheck;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StartMyServiceAtBootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent serviceIntent = new Intent(context, DetectPowerConnectedService.class);
+          //  serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startService(serviceIntent);
+        }
+    }
+}
